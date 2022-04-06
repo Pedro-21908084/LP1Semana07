abbf286666d938b864e187838450b9d2cf2ccce7
Pedro Fernandes <pm_rf@hotmail.com>
2022-04-06 18:21:37 +0100

    Correct mistake of only setting red state, & an overload for constrocotr that initializes alpha as 255

--- ColorBalls/Color.cs
+++ ColorBalls/Color.cs
@@ -10,9 +10,16 @@ namespace ColorBalls
         public Color(byte red, byte green, byte blue, byte alpha)
         {
             this.red = red;
-            this.red = green;
-            this.red = blue;
-            this.red = alpha;
+            this.green = green;
+            this.blue = blue;
+            this.alpha = alpha;
+        }
+        public Color(byte red, byte green, byte blue)
+        {
+            this.red = red;
+            this.green = green;
+            this.blue = blue;
+            this.red = 2555;
         }
     }
 }
