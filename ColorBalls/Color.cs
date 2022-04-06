b2f51fd1ec23f5871c5fb03f0891e619c4d5bec1
Pedro Fernandes <pm_rf@hotmail.com>
2022-04-06 18:20:04 +0100

    Make states, red, green, blue and alpha and make a controctor that initialize them

---
+++ ColorBalls/Color.cs
@@ -0,0 +1,18 @@
+namespace ColorBalls
+{
+    public class Color
+    {
+        private byte red;
+        private byte green;
+        private byte blue;
+        private byte alpha;
+
+        public Color(byte red, byte green, byte blue, byte alpha)
+        {
+            this.red = red;
+            this.red = green;
+            this.red = blue;
+            this.red = alpha;
+        }
+    }
+}
