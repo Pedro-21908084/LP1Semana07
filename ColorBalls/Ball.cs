2ecb74a5ad7d51527f7c42e1581705ff626be4c4
Pedro Fernandes <pm_rf@hotmail.com>
2022-04-06 18:34:16 +0100

    Make 3 cpnstroctors

--- ColorBalls/Ball.cs
+++ ColorBalls/Ball.cs
@@ -5,5 +5,21 @@ namespace ColorBalls
         private Color color;
         private float radius;
         private int throwTimes;
+
+        public Ball(Color color, float radius)
+        {
+            this.color = color;
+            this.radius = radius;
+        }
+        public Ball(float radius)
+        {
+            color = new Color(255, 0, 0);
+            this.radius = radius;
+        }
+        public Ball(float radius)
+        {
+            color = new Color(255, 0, 0);
+            radius = 1.0f;
+        }
     }
 }
